c4bef9d7a78741db87f04b287260ebfc98411842
Matt Carroll <matthewcarroll@google.com>
2018-03-23 10:55:34 -0700

    Update to use service account authentication for Google Calendar, VUI improvments
    Change-Id: Ib204f153f683f80b5c2d1c19874c881aa09fa7ff

--- README.md
+++ README.md
@@ -1,26 +1,57 @@
-# API.AI: Mary's Bike Shop using Node.js and Cloud Functions for Firebase
+# Dialogflow Bike Shop Sample
 
 ## Setup Instructions
+### Dialogflow Setup
+1. [Sign up for or sign into Dialogflow](https://console.dialogflow.com/api-client/#/login)
+1. [Create a Dialogflow agent](https://dialogflow.com/docs/getting-started/building-your-first-agent#create_an_agent)
+1. [Restore the zip file `BikeShopAgent.zip`](https://dialogflow.com/docs/agents#export_and_import)
 
-### Steps
-1. Open the API.AI console.
-1. Click *Save* to save the project.
-1. Click on the gear icon to see the project settings.
-1. Select *Export and Import*.
-1. Select *Restore from zip*. Follow the directions to restore from the `MarysBikeShop.zip` file in this directory.
-1. Create a Google Cloud project, enable the Google Calendar API and create an OAuth client IDs credentials
-1. Download the <code>client_secrets.json</code> file and save it to the <code>functions</code> directory.
-1. Run <code>npm install</code> in the <code>functions</code> directory
-1. Run <code>node acthorizeGoogleCalendar.js</code> and follow the prompts to authorize access to your calendar.
-1. Deploy the fulfillment webhook provided in the `functions` folder using [Google Cloud Functions for Firebase]
-(https://firebase.google.com/docs/functions/):
-    1. Follow the instructions to [set up and initialize Firebase SDK for Cloud Functions](https://firebase.google.com/docs/functions/get-started#set_up_and_initialize_functions_sdk). Make sure to reply `N` when asked to overwrite existing files by the Firebase CLI.
-    1. Run `firebase deploy --only functions:marysBikeShop` and take note of the endpoint where the fulfillment webhook has been published. It should look like `Function URL (marysBikeShop): https://${REGION}-${PROJECT}.cloudfunctions.net/marysBikeShop`
-1. Go back to the API.AI console and select *Fulfillment* from the left navigation menu. Enable *Webhook*, set the value of *URL* to the `Function URL` from the previous step, then click *Save*.
-1. Select *Intents* from the left navigation menu. Select the `Bike Service Intent` intent, scroll down to the end of the page and click *Fulfillment*, check *Use webhook* and then click *Save*.
+### Google Calendar Setup
+#### Service Account Setup
+1. In [Dialogflow's console](https://console.dialogflow.com), click the settings button (⚙) to go to your agent's settings. In the project ID section click on the name of the project to open the Google Cloud console for the project.
+1. In the Cloud console click on the menu button (☰), then **APIs & Services > Library**
+1. Search for and select the **Google Calendar API** and then click **Enable** to enable the API on your cloud project
+1. Next select **Credentials** on the left, then click **Create Credentials** and select **Service Account Key** from the dropdown menu.
+1. Select **New Service Account** from the dropdown and enter `bike-shop-calendar` for the name and click **Create**. In the popup, select **Create Without Role**.  A JSON file will be downloaded to your computer that you'll use in the "Bike Shop Calendar Setup" and "Fulfillment Setup" sections below.
 
-## How to make contributions?
-Please read and follow the steps in the [CONTRIBUTING.md](CONTRIBUTING.md).
+#### Bike Shop Calendar Setup
+1. Open the JSON file that was downloaded in the previous section ("Service Account Setup"), and copy the email address indicated by the `client_email` field (it should look like `bike-shop-calendar@${PROJECTID}.iam.gserviceaccount.com`)
+1. [Open Google Calendar](https://calendar.google.com) and on the left click the **+** next to **Add a friends calendar** and select **New Calendar**
+1. Enter `Bike Shop` for the name of the calendar and then click **Create Calendar**. Next, click on the bike shop calendar that will appear on the left column.
+1. Paste the email copied in the first step of this section into the **Add people** field in the **Share with specific people** section and then select **Make changes to events** in the permissions dropdown and click **Send**.
+1. Scroll down and copy the **Calendar ID** in the **Integrate Calendar** section.
+
+
+### Fulfillment Setup
+#### Add service account and Calendar ID to `index.js`
+1. Open the `functions/index.js` file in this repo
+1. Paste the **Calendar ID** copied in a "Bike Shop Calendar Setup" section into the `const calendarId = '<INSERT CALENDAR ID HERE>'` and replace `<INSERT CALENDAR ID HERE>` on line 25 of `index.js`. (the line should look similar to `const calendarId = '6ujc6j6rgfk02cp02vg6h38cs0@group.calendar.google.com'`)
+1. Next copy the contents of the JSON file downloaded in the "Service Account Setup" section and paste it into `const serviceAccount = {}` on the next line.  Paste the raw JSON, without quotes.  The JSON file will be multiple lines, the first few coouple lines should look something like this:
+
+    const serviceAccount = {
+      "type": "service_account",
+    ...
+
+1. Save the `index.js` file
+
+Choose from one of the to two options below, only one (inline editor *or* Firebase CLI) is required:
+#### Fulfillment Setup Option #1: Dialogflow Inline Editor
+1. [Enable the Cloud Function for Firebase inline editor](https://dialogflow.com/docs/fulfillment#cloud_functions_for_firebase)
+1. Copy this code in `functions/index.js` the `index.js` file in the Dialogflow Cloud Function for Firebase inline editor.
+1. Copy this code in `functions/package.json` the `package.json` file in the Dialogflow Cloud Function for Firebase inline editor.
+1. Click `Deploy`
+
+#### Fulfillment Setup Option #2: Firebase CLI
+1. `cd` to the `functions` directory
+1. Run `npm install`
+1. Install the Firebase CLI by running `npm install -g firebase-tools`
+1. Login to your Google account with `firebase login`
+1. Add your project to the sample with `firebase use [project ID]` [find your project ID here](https://dialogflow.com/docs/agents#settings)
+1. Run `firebase deploy --only functions:dialogflowFirebaseFulfillment`
+1. Paste the URL into your Dialogflow agent's fulfillment and click `Save`
+
+## Running the sample
+1. In [Dialogflow's console](https://console.dialogflow.com), in the Dialogflow simulator on the right, query your Dialogflow agent with `My bike is broken` and answer the questions your Dialogflow agent asks.  After getting the requuired information a appointment will be added to the "Bike Shop Calendar" calendar you setup earlier.
 
 ## License
 See [LICENSE](LICENSE).
--- functions/index.js
+++ functions/index.js
@@ -17,82 +17,94 @@
 'use strict';
 
 const functions = require('firebase-functions');
-const google = require('googleapis');
-const fs = require('fs');
+const {google} = require('googleapis');
+const {WebhookClient} = require('dialogflow-fulfillment');
 
-// Get user's calendar authorization information
-const clientSecrets = require('./client_secret.json');
-const clientId = clientSecrets.installed.client_id;
-const clientSecret = clientSecrets.installed.client_secret;
-const refreshToken = require('./google-calendar-auth.json').refresh_token;
+// Enter your calendar ID below and service account JSON below, see https://github.com/dialogflow/bike-shop/blob/master/README.md#calendar-setup
+const calendarId = '<INSERT CALENDAR ID HERE>'; // looks like "6ujc6j6rgfk02cp02vg6h38cs0@group.calendar.google.com"
+const serviceAccount = {}; // Starts with {"type": "service_account",...
 
-// Set the time zone
+// Set up Google Calendar Service account credentials
+const serviceAccountAuth = new google.auth.JWT({
+  email: serviceAccount.client_email,
+  key: serviceAccount.private_key,
+  scopes: 'https://www.googleapis.com/auth/calendar'
+});
+
+const calendar = google.calendar('v3');
+process.env.DEBUG = 'dialogflow:*'; // enables lib debugging statements
+
 const timeZone = 'America/Los_Angeles';
 const timeZoneOffset = '-07:00';
 
-exports.marysBikeShop = functions.https.onRequest((req, res) => {
-  // Set header for response
-  res.setHeader('Content-Type', 'application/json');
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
 
-  // Setup Google Calendar Auth
-  let OAuth2 = google.auth.OAuth2;
-  let auth = new OAuth2(clientId, clientSecret);
-  auth.setCredentials({refresh_token: refreshToken});
-  auth.refreshAccessToken(function(err, tokens) {
-    if (err) { console.log("Auth Error: Credentials not set up: " + err); }
-  });
+  function hours (agent) {
+    if (currentlyOpen()) {
+      agent.add(`We're open now! We close at 5:30pm today.`);
+    } else {
+      agent.add(`We're currently closed, but we open every weekday at 9am!`);
+    }
+  }
 
-  // Get the city and date from the request
-  let date = req.body.result.parameters['date']; // city is a required param
-  let timeStart = req.body.result.parameters['time']; // city is a required param
-  let appointmentType = req.body.result.parameters['appointment-type']; // city is a required param
+  function makeAppointment (agent) {
+    // Calculate appointment start and end datetimes (end = +1hr from start)
+    const dateTimeStart = new Date(Date.parse(agent.parameters.date + 'T' + agent.parameters.time + timeZoneOffset));
+    const dateTimeEnd = new Date(new Date(dateTimeStart).setHours(dateTimeStart.getHours() + 1));
+    const appointmentTimeString = dateTimeStart.toLocaleString(
+      'en-US',
+      { month: 'long', day: 'numeric', hour: 'numeric', timeZone: timeZone }
+    );
 
-  // Calculate appointment start and end datetimes (end = +1hr from start)
-  dateTimeStart = Date.parse(date + 'T' + timeStart + timeZone);
-  dateTimeEnd = new Date(dateTimeStart)
-  dateTimeEnd.setHours(datetimeStart.getHours() + 1 );
+    // Check the availibility of the time, and make an appointment if there is time on the calendar
+    return createCalendarEvent(dateTimeStart, dateTimeEnd).then(() => {
+      agent.add(`Ok, let me see if we can fit you in.  ${appointmentTimeString} is fine!.  Do you need a repair or just a tune-up?`);
+    }).catch(() => {
+      agent.add(`I'm sorry, there are no slots available for ${appointmentTimeString}, would you like to check another day?`);
+    });
+  }
 
-  console.log("Appointment Start: " + dateTimeStart);
-  console.log("Appointment End: " + dateTimeEnd);
-  
-  // Check calendar, if event -> reject, if no event -> create event
-  let calendar = google.calendar('v3');
-  calendar.events.list({
-  	auth: auth,
-  	calendarId: 'primary',
-  	timeMin: dateTimeStart.toISOString(),
-  	timeMax: dateTimeEnd.toISOString()
-  	}, function(err, response) {
-  	if (err) {
-  	  console.log('Error retriving calendar information: ' + err);
-  	  const errorResponse = 'Sorry, I can\'t seem to connect to my calendar, please try again later.';
-	    res.send(JSON.stringify({ 'speech': errorResponse, 'displayText': errorResponse }));
-  	}
-  	let events = response.items;
-  	// Create event if there isn't a event already there for this time period
-  	if (events.length == 0) {
-  	  console.log('No events found.');
-  
-  	  calendar.events.insert({
-  		auth: auth,
-  		calendarId: 'primary',
-  		resource: {'summary': appointmentType,
-  			'start': {'dateTime': dateTimeStart},
-  			'end': {'dateTime': dateTimeEnd}}},
-  		function(err, event) {
-  		if (err) {
-  			console.log('Error creating calendar event: ' + err);
-  			const errorResponse = 'Sorry, I can\'t seem to connect to my calendar, please try again later.';
-	    	res.send(JSON.stringify({ 'speech': errorResponse, 'displayText': errorResponse }));
-  		}
-  		console.log('Event created: %s', event.htmlLink);
-  		const successResponse = `Great! I've setup your appointment for ${dateTimeStart.toLocaleString('en-US')}.  See you then`;
-    	res.send(JSON.stringify({ 'speech': successResponse, 'displayText': successResponse }));
-  	  });
-  	} else {
-  	  console.log('Event already present in requested period');
-	    const rejectResponse = `I'm sorry, there are no slots available for ${dateTimeStart.toLocaleString('en-US')}, would you like to check another time?`;
-	    res.send(JSON.stringify({ 'speech': rejectResponse, 'displayText': rejectResponse }));
-  	}
-  });
+  let intentMap = new Map();
+  intentMap.set('Hours', hours);
+  intentMap.set('Make Appointment', makeAppointment);
+  agent.handleRequest(intentMap);
 });
+
+function currentlyOpen () {
+  var currentDateTime = new Date(); // current time
+  var hours = currentDateTime.getHours();
+  var mins = currentDateTime.getMinutes();
+  var day = currentDateTime.getDay();
+
+  return day >= 1 &&
+        day <= 5 &&
+        hours >= 9 &&
+        (hours < 17 || hours === 17 && mins <= 30);
+}
+
+function createCalendarEvent (dateTimeStart, dateTimeEnd) {
+  return new Promise((resolve, reject) => {
+    calendar.events.list({
+      auth: serviceAccountAuth, // List events for time period
+      calendarId: calendarId,
+      timeMin: dateTimeStart.toISOString(),
+      timeMax: dateTimeEnd.toISOString()
+    }, (err, calendarResponse) => {
+      // Check if there is a event already on the Bike Shop Calendar
+      if (err || calendarResponse.data.items.length > 0) {
+        reject(err || new Error('Requested time conflicts with another appointment'));
+      }
+      // Create event for the requested time period
+      calendar.events.insert({ auth: serviceAccountAuth,
+        calendarId: calendarId,
+        resource: {summary: 'Bike Appointment',
+          start: {dateTime: dateTimeStart},
+          end: {dateTime: dateTimeEnd}}
+      }, (err, event) => {
+        err ? reject(err) : resolve(event);
+      }
+      );
+    });
+  });
+}
